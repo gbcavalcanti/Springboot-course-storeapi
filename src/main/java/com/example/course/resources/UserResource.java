1ed4227138f6984ee94b240db624f69f160fa61f
gbcavalcanti <gabrielcs1097@gmail.com>
2022-09-19 09:48:18 -0300

    created user entity and resource

---
+++ src/main/java/com/example/course/resources/UserResource.java
@@ -0,0 +1,24 @@
+package com.example.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.course.entities.User;
+
+@RestController
+@RequestMapping("/users")
+public class UserResource {
+
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		
+		
+		
+		return null;
+		
+	}
+	
+}
