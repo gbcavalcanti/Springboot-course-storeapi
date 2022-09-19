6ce60845c2a29304739a6785b44ea3f4beee64ef
gbcavalcanti <gabrielcs1097@gmail.com>
2022-09-19 15:14:34 -0300

    service and component created

---
+++ src/main/java/com/example/course/services/UserService.java
@@ -0,0 +1,30 @@
+package com.example.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.course.entities.User;
+import com.example.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	
+	public User findById(Long id) {
+		
+		Optional<User> user =  userRepository.findById(id);
+		
+		return user.get();
+	}
+
+}
